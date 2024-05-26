dd347c31dd23e7fb5643b7a27b9d6cee5b33d685
Pavankumar07s <pawankumar14662693@gmail.com>
2024-05-26 11:15:24 +0530

    functions

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,19 @@ fn main() {
     print!("x:{}",x+1);
     let greet :String=String::from("hello wold");
     let char1=greet.chars().nth(1);
-    println!("{}",char1.unwrap())
+    println!("{}",char1.unwrap());
+
+    let mut a=32;
+    let inc_a =let_a_num(a);
+
+    print!("increamented a is{}",inc_a);
+    
 
 }
 
+fn let_a_num( a:i32)->i32{
+    
+    let a:i32=a+1;
+    return a;
+}
 
