9489484f3f5277a239c99ad6a5126b94ce431a8d
Pavankumar07s <pawankumar14662693@gmail.com>
2024-05-26 10:46:43 +0530

    unwrap string

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ fn main() {
     for i in 0..100{
         x=x+1;
     }
-    print!("x:{}",x+1)
+    print!("x:{}",x+1);
+    let greet :String=String::from("hello wold");
+    let char1=greet.chars().nth(1);
+    println!("{}",char1.unwrap())
+
 }
 
 
